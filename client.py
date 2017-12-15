f5a038757a1d7f1e9f935a6fcc1b536f444c058b
ryanisnan <ryanisnan@gmail.com>
2017-12-14 17:09:31 -0800

    add new data to remote call

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import grpc
 import logging
 
 # Local imports
+from matches_pb2 import CareerHistoryRating
 from matches_pb2 import CareerMatchesRequest
 from matches_pb2 import ScaleScore
 import matches_pb2_grpc
@@ -18,10 +19,17 @@ stub = matches_pb2_grpc.CareerMatchesServiceStub(channel)
 # Make a request...
 
 logging.debug('Making a request...')
-request = CareerMatchesRequest(scale_scores=[
-    ScaleScore(scale_id=1, score=1.0),
-    ScaleScore(scale_id=2, score=1.0),
-    ScaleScore(scale_id=3, score=1.0),
-])
+request = CareerMatchesRequest(
+    scale_scores=[
+        ScaleScore(scale_id=1, score=1.0),
+        ScaleScore(scale_id=2, score=1.0),
+        ScaleScore(scale_id=3, score=1.0),
+    ],
+    career_history_ratings=[
+        CareerHistoryRating(career_id=1, rating=5.0),
+        CareerHistoryRating(career_id=2, rating=5.0),
+        CareerHistoryRating(career_id=3, rating=5.0),
+    ]
+)
 matches = stub.GetMatches(request)
 logging.debug('Request completed with %s' % str(matches))
